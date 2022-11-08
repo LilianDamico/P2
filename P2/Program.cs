2cf085110a8b18ede854ba93e893976ce6f166eb
Fatec <Fatec@DESKTOP-78MS2FN>
2022-11-08 16:56:06 -0200

    Adicionar arquivos de projeto.

---
+++ P2/Program.cs
@@ -0,0 +1,21 @@
+﻿//Inicio
+int c = 1;
+int i = 1;
+while (c <= 10)
+{
+    Console.WriteLine("( "+c + "  ,");
+    while (i <= 10)
+    {
+        Console.Write(i + " ");
+        i++;
+    }
+    Console.WriteLine("" + ")");
+    i = 1;
+    c++;
+}
+
+
+
+
+Console.ReadKey();
+//Fim
